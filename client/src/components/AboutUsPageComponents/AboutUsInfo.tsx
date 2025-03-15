9aeeb51f6a592ad9a103f5e242888a8e9a0beb66
Andrew1929 <andre2904030@gmail.com>
2025-03-15 21:18:22 +0200

    stylized <about us> and <contacts> pages

--- client/src/components/AboutUsPageComponents/AboutUsInfo.tsx
+++ client/src/components/AboutUsPageComponents/AboutUsInfo.tsx
@@ -2,11 +2,13 @@ import '../../style/InfoPagesComponentsStyle/AboutUsInfo.css';
 
 export const AboutUsInfo: React.FC = () => {
     return (
-        <div className="about-us-info">
-            <h2>Про нас</h2>
-            <p>Ми команда ентузіастів, яка розробила цей сервіс для швидкого та зручного конвертування файлів.</p>
-            <p>Наша місія – зробити процес конвертації максимально простим, безпечним та ефективним.</p>
-            <p>Засновано у 2024 році командою розробників з України.</p>
+        <div className="aboutUs__content">
+            <h2 className='content__title'>Про нас</h2>
+            <div className="content__text-area">
+                <p className='content__text'>Ми команда ентузіастів, яка розробила цей сервіс для швидкого та зручного конвертування файлів.</p>
+                <p className='content__text'>Наша місія – зробити процес конвертації максимально простим, безпечним та ефективним.</p>
+                <p className='content__text'> Засновано у 2024 році командою розробників з України.</p>
+            </div>
         </div>
     );
 };
--- client/src/components/ContactsPageComponents/ContactInfo.tsx
+++ client/src/components/ContactsPageComponents/ContactInfo.tsx
@@ -2,13 +2,13 @@ import '../../style/InfoPagesComponentsStyle/ContactInfoStyle.css';
 
 export const ContactInfo: React.FC = () => {
     return (
-        <div className="contact-info">
-            <h2>Контактна інформація</h2>
-            <p>Якщо у вас є питання або пропозиції, зв'яжіться з нами:</p>
-            <ul>
-                <li>Email: support@fileconverter.com</li>
-                <li>Телефон: +380 97 123 45 67</li>
-                <li>Адреса: вул. Хрещатик, 22, Київ, Україна</li>
+        <div className="contact-info__content">
+            <h2 className='content__title'>Контактна інформація</h2>
+            <p  className='content__subtitle'>Якщо у вас є питання або пропозиції, зв'яжіться з нами:</p>
+            <ul className='content__info-list'>
+                <li className='info-list__item'>Email: support@fileconverter.com</li>
+                <li className='info-list__item'>Телефон: +380 97 123 45 67</li>
+                <li className='info-list__item'>Адреса: вул. Хрещатик, 22, Київ, Україна</li>
             </ul>
         </div>
     );
